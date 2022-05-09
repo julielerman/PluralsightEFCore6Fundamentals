4f89652e3df1241cf69796556c445ede3c2129e1
Julie Lerman <julielerman@users.noreply.github.com>
2022-05-09 09:08:32 -0400

    Update Book.cs
    This should be datetime...dateonly is only supported via a mapping

--- M02 Building Your FIrst App/Before/PublisherDomain/Book.cs
+++ M02 Building Your FIrst App/Before/PublisherDomain/Book.cs
@@ -4,7 +4,7 @@
     {
         public int BookId { get; set; }
         public string Title { get; set; }
-        public DateOnly PublishDate { get; set; }
+        public DateTime PublishDate { get; set; }
         public decimal BasePrice { get; set; }
         public Author Author { get; set; }
         public int AuthorId { get; set; }
